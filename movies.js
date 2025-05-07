6aa31c6577d895e9525209f74470da3042d44274
OleksiiTrenin <OLEKSII.TRENIN@lnu.edu.ua>
2025-05-07 20:55:56 +0300

    додав фільми

---
+++ movies.js
@@ -0,0 +1,28 @@
+const movies = [
+  {
+    id: 1,
+    title: "Top Gun: Maverick",
+    description: "After more than thirty years of service, Maverick trains a new generation of pilots.",
+    genre: "Action",
+    poster: require("../assets/images/top-gun-maverick.jpg"),
+    showtime: "2025-05-02 16:00",
+  },
+  {
+    id: 2,
+    title: "The Matrix",
+    description: "A hacker discovers a mysterious truth about reality.",
+    genre: "Action",
+    poster: require("../assets/images/matrix.jpg"),
+    showtime: "2025-05-01 20:00",
+  },
+  {
+    id: 3,
+    title: "Interstellar",
+    description: "A team of explorers travel through a wormhole in space.",
+    genre: "Sci-Fi",
+    poster: require("../assets/images/interstellar.jpg"),
+    showtime: "2025-05-01 22:00",
+ 
+];
+
+export default movies;
